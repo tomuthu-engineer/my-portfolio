0563d47eaedca8560079f2cb5429d8dbc0bb92b8
tomuthu-engineer <tomuthu.engineer@gmail.com>
2024-09-18 23:52:06 +0530

    home screen fixed

--- components/ui/Photo.tsx
+++ components/ui/Photo.tsx
@@ -18,12 +18,12 @@ const Photo = () => {
                         opacity: 1,
                         transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" }
                     }}
-                    className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute'>
+                    className='w-[298px] h-[298px] xl:w-[398px] xl:h-[398px] mix-blend-lighten absolute'>
                     <Image src="/assets/photo.jpeg  " quality={100} fill alt='' className='object-contain' />
                 </motion.div>
             </motion.div>
 
-            <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+            <motion.svg className="w-[300px] xl:w-[406px] h-[300px] xl:h-[406px]"
             fill="transparent"
             viewBox="0 0 506 506"
             xmlns="http://www.w3.org/2000/svg">
